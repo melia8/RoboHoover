7a496fcdf3593e336c12c316de99b4fa84acdd3a
Paul Melia <p.melia@gre.ac.uk>
2017-08-20 18:14:20 +0100

    added a specific database name and port

--- src/main/java/com/melia/yoti/robohoover/RobohooverApplication.java
+++ src/main/java/com/melia/yoti/robohoover/RobohooverApplication.java
@@ -1,12 +1,26 @@
 package com.melia.yoti.robohoover;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class RobohooverApplication {
+public class RobohooverApplication extends AbstractMongoConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "YotiHooverDb";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient("127.0.0.1", 27017);
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RobohooverApplication.class, args);
